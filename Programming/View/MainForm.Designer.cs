c2346ce98fbe7591a3cbeee000c537be64252a38
LarionovKirill <lari-kir2013@mail.ru>
2023-04-03 19:04:31 +0700

    Feature(MainForm.Designer.cs): Добавил адаптивную верстку канвы.

--- Programming/View/MainForm.Designer.cs
+++ Programming/View/MainForm.Designer.cs
@@ -750,6 +750,9 @@ namespace Programming.View
             // 
             // panelForRectangles
             // 
+            this.panelForRectangles.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.panelForRectangles.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.panelForRectangles.Location = new System.Drawing.Point(442, 6);
             this.panelForRectangles.Name = "panelForRectangles";
