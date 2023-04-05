babb513b7ac43cb348005157886f919d922be76e
LarionovKirill <lari-kir2013@mail.ru>
2023-04-05 20:33:33 +0700

    Fix(RectangleFactory.cs): Оформил по RSDN + добавил комментарий, который забыл.

--- Programming/Model/Geometry/RectangleFactory.cs
+++ Programming/Model/Geometry/RectangleFactory.cs
@@ -8,19 +8,25 @@ namespace Programming.Model
 {
     static class RectangleFactory
     {
+        /// <summary>
+        /// Создает случайный прямоугольник, который не выйжет за пределы канвы. 
+        /// </summary>
+        /// <param name="maxWidth"> Максимальная ширина канвы.</param>
+        /// <param name="maxHeight">Максимальная выстока канвы.</param>
+        /// <returns>Возвращает сгенерированный прямоугольник.</returns>
         static public Rectangle Randomize(int maxWidth, int maxHeight)
         {
             Random random = new Random();
-            Rectangle _currentRectangle = new Rectangle();
-            _currentRectangle.Length = random.Next(5, 101);
-            int heigth = int.Parse(_currentRectangle.Length.ToString());
-            _currentRectangle.Width = random.Next(5, 101);
-            int width = int.Parse(_currentRectangle.Width.ToString());
+            Rectangle currentRectangle = new Rectangle();
+            currentRectangle.Length = random.Next(5, 101);
+            int heigth = int.Parse(currentRectangle.Length.ToString());
+            currentRectangle.Width = random.Next(5, 101);
+            int width = int.Parse(currentRectangle.Width.ToString());
             int x = random.Next(15, maxWidth - 15 - width);
             int y = random.Next(15, maxHeight - 15 - heigth);
             Point2D point = new Point2D(x,y);
-            _currentRectangle.Center = point;
-            return _currentRectangle;
+            currentRectangle.Center = point;
+            return currentRectangle;
         }
     }
 }
