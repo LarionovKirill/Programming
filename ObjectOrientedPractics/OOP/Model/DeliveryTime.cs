c50f5eb470f2b02f53141b0a3982c4850d0646b8
LarionovKirill <lari-kir2013@mail.ru>
2023-05-12 21:07:53 +0700

    Feature(DelivaryTime.cs): создал перечисление в интервалами времени доставки. Feature(PriorityOrder.cs): создал класс приоритетных заказов.

---
+++ ObjectOrientedPractics/OOP/Model/DeliveryTime.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP.Model
+{
+    /// <summary>
+    /// Время доставки.
+    /// </summary>
+    public enum DeliveryTime
+    {
+        Between9And11,
+        Between11And13,
+        Between13And15,
+        Between15And17,
+        Between17And19,
+        Between19And21,
+        Between21And23
+    }
+}
---
+++ ObjectOrientedPractics/OOP/Model/PriorityOrder.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP.Model
+{
+    /// <summary>
+    /// Класс хранит приоритетные заказы.
+    /// Наследуется от класса
+    /// <see cref="Order"></see>
+    /// </summary>
+    class PriorityOrder : Order
+    {
+        /// <summary>
+        /// Свойство времени доставки заказа.
+        /// </summary>
+        public DeliveryTime DeliveryTime { get; set; }
+
+        /// <summary>
+        /// Свойство даты достваки заказа.
+        /// </summary>
+        public DateTime DeliveryDay { get; set; }
+
+        /// <summary>
+        /// Конструктор без параметров, унаследованный от родительского класса.
+        /// </summary>
+        public PriorityOrder() : base()
+        {
+            
+        }
+
+        /// <summary>
+        /// Конструктор класса с параметрами.
+        /// </summary>
+        /// <param name="address">Адрес доставки.</param>
+        /// <param name="orderStatus">Статус заказа.</param>
+        /// <param name="creatingDate">Дата создания заказа.</param>
+        /// <param name="deliveryTime">Время доставки.</param>
+        /// <param name="deliveryDay">Дата доставки.</param>
+        public PriorityOrder(
+            Address address,
+            OrderStatus orderStatus,
+            DateTime creatingDate,
+            DeliveryTime deliveryTime, 
+            DateTime deliveryDay): base(creatingDate,address, orderStatus)
+        {
+            this.DeliveryDay = deliveryDay;
+            this.DeliveryTime = deliveryTime;
+            /*this.Address = address;
+            this.DateOfCreate = creatingDate;
+            this.OrderStatus = orderStatus;*/
+        }
+
+    }
+}
