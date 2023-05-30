e88f6be24c3293ac6a43d6ab8bd4d89dd390101a
LarionovKirill <lari-kir2013@mail.ru>
2023-05-30 10:45:26 +0700

    Feature(DataTools.cs): создал сервисный класс и добавил в него метод сортировки по цене.

---
+++ ObjectOrientedPractics/OOP/Services/DataTools.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OOP.Model;
+
+namespace OOP.Services
+{
+    static class DataTools
+    {
+        /// <summary>
+        /// Метод принимает список товаров и возвращает товары стоимостью более 5000.
+        /// </summary>
+        /// <param name="items">Исходный список.</param>
+        /// <returns>Список с товарами, ценою больше 5000.</returns>
+        static List<Item> FilteringProducts(List<Item> items)
+        {
+            List<Item> filteringList = new List<Item>();
+            foreach(var item in items)
+            {
+                if (item.Cost >= 5000)
+                {
+                    filteringList.Add(item);
+                }
+            }
+            return filteringList;
+        }
+    }
+}
