5c48253f42e38b844ef12ec59255602efa7cd73f
LarionovKirill <lari-kir2013@mail.ru>
2023-05-30 10:54:55 +0700

    Feature(DataTools.cs): добавил метод фильтрации по категории товара.

--- ObjectOrientedPractics/OOP/Services/DataTools.cs
+++ ObjectOrientedPractics/OOP/Services/DataTools.cs
@@ -3,9 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using OOP.Model;
+using OOP.Model.Enums;
 
 namespace OOP.Services
 {
+    /// <summary>
+    /// Сервисный класс обработки данных товара.
+    /// </summary>
     static class DataTools
     {
         /// <summary>
@@ -19,6 +23,25 @@ namespace OOP.Services
             foreach(var item in items)
             {
                 if (item.Cost >= 5000)
+                {
+                    filteringList.Add(item);
+                }
+            }
+            return filteringList;
+        }
+
+        /// <summary>
+        /// Метод принимает список товаров с ворзвращает список товаров определенной категории.
+        /// </summary>
+        /// <param name="items">Исходный список.</param>
+        /// <returns>Список товаров выбранной категории.</returns>
+        static List<Item> FilteringByCategory(List<Item> items)
+        {
+            var choosenCategory = ItemCategory.Elictronics;
+            List<Item> filteringList = new List<Item>();
+            foreach (var item in items)
+            {
+                if (item.ItemCategory == choosenCategory)
                 {
                     filteringList.Add(item);
                 }
